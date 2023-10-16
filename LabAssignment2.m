0ed56ff7b76fc9d42455d7e6ccaa762021afdc1d
alext <alext@172.19.117.160>
2023-10-16 15:54:33 +1100

    UR5 robot moving plates with jtraj. Animations happening seperately

--- LabAssignment2.m
+++ LabAssignment2.m
@@ -28,24 +28,24 @@ classdef LabAssignment2 < handle
             clc;
 
             hold on
-            self.initialiseRobots();
-            self.initialiseEnvironment();
-            self.initialisePlates();
-            self.runRobot();            
+            self.InitialiseRobots();
+            self.InitialiseEnvironment();
+            self.InitialisePlates();
+            self.RunRobot();            
         end
         
-        function initialiseRobots(self)
+        function InitialiseRobots(self)
             self.panda = Panda(transl(1.5, 2.6, 1.0));
 
             %%  below change to the new linear ur5 (with gripper attachment
-            self.linearUR5 = LinearUR5custom(transl(0.0,2.7,1.0));
+            self.linearUR5 = LinearUR5custom(transl(0.5,2.6,1.0));
         end
 
-        function initialiseEnvironment(self)
+        function InitialiseEnvironment(self)
             Environment(); % Build the workspace for the robot
         end
 
-        function initialisePlates(self)
+        function InitialisePlates(self)
             self.objPlates = InitialisePlates(); % plot the plates in the workspace
 
             plateNum = 1;
@@ -66,9 +66,9 @@ classdef LabAssignment2 < handle
             end
         end
 
-        function runRobot(self)                      
+        function RunRobot(self)                      
             input('Press enter to begin')
-            self.updateRobots();
+            self.UpdateRobots();
         
             % Initialise log variable
             logData = struct('Time', [], 'Status', [], 'Transform', []); % Create the logData Structure
@@ -79,12 +79,14 @@ classdef LabAssignment2 < handle
             logData.Status{end+1} = 'Task Started';
             logData.Transform{end+1} = 'N/A';
 
+            stack = 1;
+
             for i = 1:self.objPlates.numOfPlates
                 disp(['Panda unstacking plate ', num2str(i)])
                 self.pandaState = 1;
                 self.ur5State = 1;
-                ur5Started = false;
                 self.plateModel = self.plates{self.plateCounter};
+                % self.movePanda();
                 for j = 1:6
                     % STATE 1 is panda moving to safe position above initial plate position (WITHOUT plate)
                     % STATE 2 is panda picking up plate from initial plate position
@@ -99,13 +101,13 @@ classdef LabAssignment2 < handle
                     logData.Status{end+1} = ['Panda picking/placing plate ', num2str(i), ' in State ', num2str(j)];
                     logData.Transform{end+1} = mat2str(self.objPlates.initialTargetTransforms{i}(1:3, 4).');
 
-                    self.moveToPos();
+                    self.MoveToPos();
 
-                    if i == 4 && ~ur5Started
-                        self.moveUR5(1)
-                        ur5Started = true;
+                    if (i == 3 && self.pandaState == 6) || (i == 6 && self.pandaState == 6) || (i == 9 && self.pandaState == 6)
+                        self.MoveUR5(stack)
+                        stack = stack + 3;
                     end
-                    
+
                     self.pandaState = self.pandaState + 1;
                 end
                 self.plateCounter = self.plateCounter + 1;
@@ -121,8 +123,8 @@ classdef LabAssignment2 < handle
             save('logData.mat', 'logData');
         end
 
-        function moveToPos(self)
-            self.updateRobots();
+        function MoveToPos(self)
+            self.UpdateRobots();
             steps = 10;
             switch self.pandaState
                 case 1
@@ -159,15 +161,15 @@ classdef LabAssignment2 < handle
             robotXYZ = robotXYZ(1:3,4)';
             targetXYZ = targetPos(1:3,4)';
             
-            cartesianPath = self.calculateMidpoints(robotXYZ, targetXYZ, steps);
+            cartesianPath = self.CalculateMidpoints(robotXYZ, targetXYZ, steps);
 
             for i = 1:length(cartesianPath)
                 t = transl(cartesianPath(i,:)) * rpy;
                 pandaAngles = self.panda.model.ikcon(t, self.pandaJointAngles);
                 self.panda.model.animate(pandaAngles);
-                self.updateRobots();
+                self.UpdateRobots();
                 if self.pandaState == 3 || self.pandaState == 4 || self.pandaState == 5
-                    self.movePlates();
+                    self.MovePlates();
                 end
                 drawnow();
                 % pause(0.1);
@@ -179,19 +181,19 @@ classdef LabAssignment2 < handle
             disp(['distance from target = ', num2str(norm(realXYZ - targetXYZ)*1000), 'mm'])
         end
 
-        function cartesianPath = calculateMidpoints(self, robotXYZ, targetXYZ, steps)
+        function CartesianPath = CalculateMidpoints(self, robotXYZ, targetXYZ, steps)
             initialXYZ = robotXYZ; % Get the XYZ coordinates from the robots end effector position
             finalXYZ = targetXYZ; % Get the XYZ coordinates from the final position transformation matrix
 
             if self.pandaState == 2 || self.pandaState == 3 || self.pandaState == 5 || self.pandaState == 6
                 % Interpolate between robotXYZ and targetXYZ
-                cartesianPath = zeros(steps, length(initialXYZ));
+                CartesianPath = zeros(steps, length(initialXYZ));
                 % Generate the path
                 for i = 1:length(initialXYZ)
-                    cartesianPath(:, i) = linspace(initialXYZ(i), finalXYZ(i), steps);
+                    CartesianPath(:, i) = linspace(initialXYZ(i), finalXYZ(i), steps);
                 end
             else
-                cartesianPath = zeros(steps, 3);
+                CartesianPath = zeros(steps, 3);
                 midpoint = (initialXYZ + finalXYZ) / 2; % Calculate the midpoint between the initial and final positions
                 vec = finalXYZ - initialXYZ; % Calculate the vector from the initial to the final position
                 offset_distance = 0.75 - 0.25 * abs(midpoint(2)) / 1;  % Calculate the offset distance based on the Y coordinate of the midpoint
@@ -205,35 +207,52 @@ classdef LabAssignment2 < handle
                     oneMinusT = 1 - t; % Calculate (1 - t) for use in the Bezier curve formula
                     % Use the formula for a quadratic Bezier curve to generate the arc
                     bezier_t = oneMinusT^2 * initialXYZ + 2 * oneMinusT * t * crest + t^2 * finalXYZ; % Calculate the position on the Bezier curve at parameter t
-                    cartesianPath(i, :) = bezier_t; % Assign the calculated position to the Cartesian path matrix
+                    CartesianPath(i, :) = bezier_t; % Assign the calculated position to the Cartesian path matrix
                 end
             end
         end
 
-        function moveUR5(self, stack)
-            initialTarget = self.objPlates.stackTargetTransforms{stack}
-            finalTarget = self.objPlates.finalTargetTransforms{stack}
-            finalJoints = self.linearUR5.model.ikcon(finalTarget, self.ur5JointAngles)
+        function MoveUR5(self, stack)
+            stack
+            targetTransforms = cell(7);
+            targetTransforms{1} = self.objPlates.stackTargetTransforms{stack} * rpy2tr(0, 90, 0, 'deg');
+            targetTransforms{2} = targetTransforms{1};
+            targetTransforms{4} = targetTransforms{1};
+            targetTransforms{1}(1,4) = targetTransforms{1}(1,4) - 0.1;
+            targetTransforms{3} = targetTransforms{1};
+            targetTransforms{4}(3,4) = targetTransforms{4}(3,4) + 0.45;
+            targetTransforms{5} = self.objPlates.finalTargetTransforms{stack} * rpy2tr(0, 90, -90, 'deg');
+            targetTransforms{6} = targetTransforms{5};
+            targetTransforms{5}(2,4) = targetTransforms{5}(2,4) + 0.1;
+            targetTransforms{7} = targetTransforms{5};
 
-            qMatrix = jtraj(self.ur5JointAngles, finalJoints, 10)
-            
-            for i = 1:length(qMatrix)
-                q = qMatrix(i,:)
-                self.linearUR5.model.animate(q)
-                drawnow()
-                pause(0.1);
+            for i = 1:length(targetTransforms)
+                angles = self.linearUR5.model.ikcon(targetTransforms{i}, self.ur5JointAngles);
+                qMatrix = jtraj(self.ur5JointAngles, angles, 10);
+                for j = 1:length(qMatrix)
+                    q = qMatrix(j,:);
+                    self.linearUR5.model.animate(q);
+                    drawnow();
+                    pause(0.1);
+                end
+                self.UpdateRobots();
             end
         end
 
+        function SplitAnimation(self, qMatrixPanda, qMatrixUR5)
+            for i = 1:length(qMatrixPanda)
+                q = qMatrixPanda()
+            end
+        end
 
-        function updateRobots(self)
+        function UpdateRobots(self)
             self.pandaJointAngles = self.panda.model.getpos();
             self.pandaEnd = self.panda.model.fkine(self.pandaJointAngles);
             self.ur5JointAngles = self.linearUR5.model.getpos();
             self.ur5End = self.linearUR5.model.fkine(self.ur5JointAngles);
         end
 
-        function movePlates(self)
+        function MovePlates(self)
             platePos = self.pandaEnd.T;
         
             % Extract the current z-direction of the end effector

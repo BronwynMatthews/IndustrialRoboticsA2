82b6a13200709bba6d88ee3134ee7a294b312399
Alex Tweed <alexander.w.tweed@student.uts.edu.au>
2023-10-29 12:29:09 +1100

    Real robot joint states working correctly

--- RealRobotTest.m
+++ RealRobotTest.m
@@ -6,7 +6,7 @@ robot = UR3;
 n = robot.model.n;
 
 
-axis([-0.5 0.5 -0.5 0.6 0.0 0.6]);
+axis([-0.5 0.5 -0.5 0.6 0.0 0.8]);
 hold on
 
 q0 = [0 -90 0 -90 -90 0];
@@ -17,9 +17,9 @@ robot.model.animate(q0);
 plateOffset = 0.1;
 
 initialStack = {
-    [0.3 0.2 0.1]; 
-    [0.4 0.0 0.05];
-    [0.4 -0.2 0.05];
+    [0.2 0.2 0.1]; 
+    [0.5 0.1 0.1];
+    [0.5 0.2 0.1];
 };
 
 trStack = cell(3,1);
@@ -28,9 +28,9 @@ for i = 1:length(trStack)
 end
 
 finalStack = {
-    [0.2, -0.4, 0.3];
-    [0.0, -0.4, 0.3];
-    [-0.2, -0.4, 0.3];
+    [0.2, -0.5, 0.3];
+    [0.0, -0.5, 0.3];
+    [-0.2, -0.5, 0.3];
 };
 
 trFinal = cell(3,1);
@@ -40,92 +40,99 @@ end
 
 
 
-targetTransforms = cell(21,1);
+targetTransforms = cell(20,1);
 stack = 1;
 
-% for i = 1:length(trStack)
-%     targetTransforms{1} = trStack{1} * rpy2tr(90, -180, 90, 'deg');
-%     % targetTransforms{1*i} = targetTransforms{1*i} * rpy2tr(90, -180,90, 'deg');
-%     % targetTransforms{1*i}(1,4) = targetTransforms{1*i}(1,4) - plateOffset - 0.2;
-%     % targetTransforms{1*i} = targetTransforms{1*i} * rpy2tr(90, -180, 90, 'deg');
-% 
-%     targetTransforms{2*i} = trStack{i} * rpy2tr(0, -90, 0, 'deg');
-%     targetTransforms{2*i}(1,4) = targetTransforms{2*i}(1,4) - plateOffset;
-% 
-%     targetTransforms{3*i} = targetTransforms{2*i} * rpy2tr(0, 0, -90, 'deg');
-%     targetTransforms{3*i}(3,4) = targetTransforms{3*i}(3,4) + 0.25;
-% 
-%     targetTransforms{4*i} = trFinal{stack} * rpy2tr(0, -90, 90, 'deg');
-%     targetTransforms{4*i}(2,4) = targetTransforms{4*i}(2,4) + plateOffset + 0.2;
-% 
-%     targetTransforms{5*i} = trFinal{stack} * rpy2tr(0, -90, 90, 'deg');
-%     targetTransforms{5*i}(2,4) = targetTransforms{5*i}(2,4) + plateOffset;
-% 
-%     targetTransforms{6*i} = targetTransforms{4*i};
-% 
-%     targetTransforms{7*i} = robot.model.fkine(q0);
-% 
-%     stack = stack + 1;
-% 
-%     % for i = 1:7
-%     %     targetTransforms{i}
-%     % end
-%     % disp('new stack')
-% end
+for i = 0:2
+    targetTransforms{1+(7*i)} = trStack{stack} * rpy2tr(-90, -180, -90, 'deg');
+    targetTransforms{1+(7*i)}(1,4) = targetTransforms{1+(7*i)}(1,4) - plateOffset - 0.2;
 
+    targetTransforms{2+(7*i)} = trStack{stack} * rpy2tr(-90, -180, -90, 'deg');
+    targetTransforms{2+(7*i)}(1,4) = targetTransforms{2+(7*i)}(1,4) - plateOffset;
 
-tr = cell(729,1);
-count = 1;
-for i = -180:45:180
-    for j = -180:45:180
-        for k = -180:45:180
-            tr{count} = trStack{1} * rpy2tr(k,j,i, 'deg');
-            
-            jointAngles = robot.model.ikcon(tr{count}, q0);
-            xyz = robot.model.fkine(jointAngles);
-            xyz = xyz.T;
-            xyz = xyz(1:3,4)';
-            dist = norm(xyz - initialStack{1});
-            if dist <= 0.005
-                robot.model.animate(jointAngles)
+    targetTransforms{3+(7*i)} = targetTransforms{2+(7*i)} * rpy2tr(90, -90, -90, 'deg');
+    targetTransforms{3+(7*i)}(3,4) = targetTransforms{3+(7*i)}(3,4) + 0.2;
 
-                drawnow();
-                pause(1)
+    targetTransforms{4+(7*i)} = trFinal{stack} * rpy2tr(90, 0, 0, 'deg');
+    targetTransforms{4+(7*i)}(2,4) = targetTransforms{4+(7*i)}(2,4) + plateOffset + 0.2;
 
-                disp(['count = ', num2str(count), ' r = ', num2str(k), ' p = ', num2str(j),' y = ', num2str(i)]);
-            end 
-            count = count + 1;
-        end
+    targetTransforms{5+(7*i)} = trFinal{stack} * rpy2tr(90, 0, 0, 'deg');
+    targetTransforms{5+(7*i)}(2,4) = targetTransforms{5+(7*i)}(2,4) + plateOffset;
+
+    targetTransforms{6+(7*i)} = targetTransforms{4+(7*i)};
+
+    if 7 + (7 * i) == 21
+        break
+    else
+        targetTransforms{7+(7*i)} = robot.model.fkine(q0);
+        targetTransforms{7+(7*i)} = targetTransforms{7+(7*i)}.T;
+    
+        stack = stack + 1;
     end
+
+    for i = 1:7
+        targetTransforms{i}
+    end
+    disp('new stack')
 end
 
 
-%%
+% tr = cell(729,1);
+% count = 1;
+% for i = -180:45:180
+%     for j = -180:45:180
+%         for k = -180:45:180
+%             tr{count} = trStack{1} * rpy2tr(k,j,i, 'deg');
+% 
+%             jointAngles = robot.model.ikcon(tr{count}, q0);
+%             xyz = robot.model.fkine(jointAngles);
+%             xyz = xyz.T;
+%             xyz = xyz(1:3,4)';
+%             dist = norm(xyz - initialStack{1});
+%             if dist <= 0.005
+%                 robot.model.animate(jointAngles)
+% 
+%                 drawnow();
+%                 disp(['count = ', num2str(count), ' r = ', num2str(k), ' p = ', num2str(j),' y = ', num2str(i)]);
+%                 pause(1)
+% 
+% 
+%             end 
+%             count = count + 1;
+%         end
+%     end
+% end
 
 steps = 10;
 
 qMatrix = zeros(steps, n);
 
-robot = UR3;
+% robot = UR3;
 
-% for i = 1:length(targetTransforms)
-for i = 1:1
-    i
-    tr = trStack{1} * rpy2tr(-135, 0, -90, 'deg');
-    jointAngles = robot.model.ikcon(tr, q0);
-    robot.model.teach(jointAngles)
-    % qMatrix = jtraj(q0, jointAngles, 10);
-    % qMatrix = ResolvedMotionRateControl(robot, tr)
-    % for j = 1:length(qMatrix)
-    %     robot.model.animate(qMatrix(j, :)); 
-    %     drawnow();
-    % 
-    %     pause(0.5);
-    % end
+realJointStates = cell(20,1);
+
+for i = 1:length(targetTransforms)
+% for i = 1:1
+    tr = targetTransforms{i};
+    jointAngles = robot.model.ikcon(tr, robot.model.getpos());
+    realJointStates{i} = jointAngles;
+    % robot.model.teach(jointAngles)
+    if i == 2 || i == 5 || i == 16  || i == 19 
+        qMatrix = ResolvedMotionRateControl(robot, tr);
+    else
+        qMatrix = jtraj(robot.model.getpos(), jointAngles, 10);
+    end
+    for j = 1:length(qMatrix)
+        robot.model.animate(qMatrix(j, :)); 
+        drawnow();
+
+        pause(0.1);
+    end
     % robot.model.animate(jointAngles)
     xyz = robot.model.fkine(robot.model.getpos());
-    xyz = xyz.T;
-    xyz = xyz(1:3,4)'
+    rpy = tr2rpy(xyz);
+    round(rad2deg(rpy))
+    pause(1)
 end
 
 
@@ -161,7 +168,7 @@ goal.Trajectory.JointNames = jointNames;
     startJointSend.TimeFromStart = rosduration(0);     
       
     endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
-    nextJointState_123456 = qMatrix(1,:); %% INPUT JOINT NUMBER YOU WISH TO TEST HERE
+    nextJointState_123456 = realJointStates(1,:); %% INPUT JOINT NUMBER YOU WISH TO TEST HERE
     endJointSend.Positions = nextJointState_123456;
     endJointSend.TimeFromStart = rosduration(durationSeconds);
     
@@ -175,7 +182,7 @@ goal.Trajectory.JointNames = jointNames;
     sendGoal(client,goal);
 
     %% RUN THE WHOLE ROBOT PROGRAM
-for i = 1:length(initialStack)
+for i = 1:length(realJointStates)
     % Query Current joint staterosh
     
     pause(2); % Pause to give time for a message to appear
@@ -196,7 +203,7 @@ for i = 1:length(initialStack)
     startJointSend.TimeFromStart = rosduration(0);     
           
     endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
-    nextJointState_123456 = qMatrix(i,:);
+    nextJointState_123456 = realJointStates(i,:);
     endJointSend.Positions = nextJointState_123456;
     endJointSend.TimeFromStart = rosduration(durationSeconds);
     
